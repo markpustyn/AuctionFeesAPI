861754ac43ef6b4b89531b80637036da181539c3
markpustyn <108764244+markpustyn@users.noreply.github.com>
2026-04-17 15:46:17 -0700

    added rate limting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { FeesController } from './fees/fees.controller';
 import { FeesService } from './app.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   controllers: [FeesController],
-  providers: [FeesService],
+  providers: [
+    FeesService, 
+  {      provide: APP_GUARD,
+        useClass: ThrottlerGuard
+  }
+  ],
+    imports: [
+     ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 60
+        },
+      ],
+    }),
+  ],
 })
 export class FeesModule {}
